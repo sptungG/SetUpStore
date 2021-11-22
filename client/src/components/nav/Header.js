1118aab6636eced198864b83998ec0ec40afd65e
sptungG <ngoviettung154@gmail.com>
2021-11-22 08:08:02 +0700

    update style header

--- client/src/components/nav/Header.js
+++ client/src/components/nav/Header.js
@@ -2,41 +2,40 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { FaSearch, FaShoppingCart } from "react-icons/fa";
 
-const Header = () => {
+export default function Header() {
   const [current, setCurrent] = useState("home");
 
   const handleClick = (e) => {
-    console.log(e.key);
-    setCurrent(e.key);
+    console.log(e.target.dataset.key ?? "");
+    // setCurrent(e.dataset.key);
   };
 
   return (
     <header className="header" onClick={handleClick}>
       <div className="container">
-        <div key="home" className="header-left">
+        <div className="header-left">
           <img src="./index.svg" alt="logo" />
-          <Link to="/"><span>SetUpStore</span></Link>
+          <Link data-key="home" to="/">SetUpStore</Link>
         </div>
         <div className="header-center">
           <form>
             <input type="email" placeholder="Type your product ..." />
-            <button><FaSearch /></button>
+            <button>
+              <FaSearch />
+            </button>
           </form>
         </div>
         <div className="header-right">
-          <div key="login" className="btn btn-login">
-            <Link to="/login">Login</Link>
-          </div>
-          <div key="register" className="btn btn-register">
-            <Link to="/register">Register</Link>
-          </div>
-          <div key="cart" className="cart">
-            <Link to="/"><FaShoppingCart size={28}/></Link>
+          <Link data-key="login" to="/login" className="btn btn-login" >Login</Link>
+          <Link data-key="register" to="/register" className="btn btn-register">Register</Link>
+          <div className="cart">
+            <Link data-key="cart" to="/">
+              <FaShoppingCart size={28} />
+            </Link>
             <div className="cart-badge">1</div>
           </div>
         </div>
       </div>
     </header>
   );
-};
-export default Header;
+}
