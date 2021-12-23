7bd470b979617bc6f92fcade550059096c793a8d
lmhdev <lemh92@gmail.com>
2021-12-23 22:12:05 +0700

    save change

--- client/src/pages/checkout/Checkout.js
+++ client/src/pages/checkout/Checkout.js
@@ -18,6 +18,7 @@ const Checkout = () => {
       "https://grovemade.com/static/thumbnails/shop/variant/walnut-ipad-stand-gridA-A3_400x400_85.jpg?_v=1603835220.37",
     productName: "Wood iPad Stand",
     productPrice: "90",
+    shippingFee: "15",
   };
 
   const handleQuantityChange = (value) => {
@@ -119,10 +120,58 @@ const Checkout = () => {
             </Form>
           </div>
           <Divider type="vertical" style={{ height: "100vh" }} />
+
+          <div style={{ width: "40%", padding: "1rem" }}>
+            <div style={{ display: "flex", flexGrow: "1" }}>
+              <Image width={100} src={PROPS.productImg} />
+              <div style={{ display: "flex", justifyContent: "space-between" }}>
+                <Typography.Text>{PROPS.productName}</Typography.Text>
+                <Typography.Text>${PROPS.productPrice}</Typography.Text>
+              </div>
+            </div>
+          </div>
+        </Layout>
+      )}
+
+      {currentScreen === "payment" && (
+        <Layout
+          style={{
+            width: "100%",
+            height: "100vh",
+            display: "flex",
+            flexDirection: "row",
+          }}
+        >
+          <div style={{ width: "60%", padding: "1rem 3rem" }}>
+            <Form layout="vertical">
+              <Form.Item label="Payment">
+                <Input placeholder="Card Number" />
+                <Input placeholder="Name on Card" />
+                <Input placeholder="Expiration Date (DD/MM)" />
+              </Form.Item>
+              <Form.Item label="Billing Address">
+                <Input placeholder="City" />
+                <Input placeholder="Address Line 1" />
+                <Input placeholder="Address Line 2 (optional)" />
+              </Form.Item>
+              <div style={{ display: "flex" }}>
+                <Button
+                  type="primary"
+                  onClick={() => {}}
+                  style={{ marginRight: "1rem" }}
+                >
+                  Pay Now
+                </Button>
+                <Button onClick={() => setCurrentScreen("info")}>
+                  Return to Info
+                </Button>
+              </div>
+            </Form>
+          </div>
+          <Divider type="vertical" style={{ height: "100vh" }} />
           <div style={{ width: "40%", padding: "1rem" }}>Content</div>
         </Layout>
       )}
-      {currentScreen === "payment" && <div>Payment</div>}
     </>
   );
 };
