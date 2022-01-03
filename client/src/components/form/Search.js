41defbe83fa229e28d2a6112c4697bfcbd6755a1
sptungG <ngoviettung154@gmail.com>
2022-01-03 10:26:12 +0700

    update header

--- client/src/components/form/Search.js
+++ client/src/components/form/Search.js
@@ -27,7 +27,12 @@ function Search({ affixed }) {
     <Row align="middle" justify="center">
       <form name="header-search" style={{ width: "100%", lineHeight: 1.4 }} onSubmit={handleSubmit}>
         <Input
-          style={{ borderRadius: 100, padding: "5px 8px 5px 20px", backgroundColor: affixed ? "transparent" : "rgba(245, 103, 102, 0.05)" }}
+          style={{
+            borderRadius: 100,
+            padding: "5px 8px 5px 20px",
+            backgroundColor: affixed ? "transparent" : "rgba(245, 103, 102, 0.05)",
+            borderColor: affixed ? "#d9d9d9" : "transparent",
+          }}
           allowClear
           placeholder="Type your product ..."
           value={text}
